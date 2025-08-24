bed8273908df35b3b606788c4652df7387e704e2
IsaquePFerreira <92412320+IsaquePFerreira@users.noreply.github.com>
2025-08-23 21:59:17 -0300

    Update ppi.sh

--- ppi.sh
+++ ppi.sh
@@ -4,115 +4,143 @@
 # Faz a pós-instalação do Void Linux.
 #
 
+set -e # Sai em caso de erro
+
 # Variáveis
-# Artwork
-URL_WALLPAPERS="https://github.com/IsaquePFerreira/wallpapers"
-WALLPAPERS_FOLDER="$HOME/Pictures/wallpapers"
-URL_FONTS="https://github.com/IsaquePFerreira/fonts"
-FONTS_FOLDER="$HOME/.local/share/fonts"
-# Pega o nome do usuário
-USERNAME="$(logname)"
+USERNAME="$(logname)" # Pega o nome do usuário
+SUCKLESS_DIR="$HOME/.config/suckless" # Diretório programas suckless
 PACKAGES=(
-"build-essential"
+"linux-headers"
+"xorg-minimal"
+"xf86-video-intel"
+"xorg-input-drivers"
+"base-devel"
+"libX11-devel"
+"libXft-devel"
+"libXinerama-devel"
+"man-db"
+"dbus"
+"elogind"
+"polkit"
+"ufw"
 "chromium"
 "curl"
 "wget"
-"docker.io"
+"docker"
 "fzf"
 "gimp"
+"ffmpeg"
 "git"
 "htop"
 "inxi"
 "lynx"
 "w3m"
 "ncdu"
+"feh"
+"mpv"
+"mupdf"
+"ranger"
+"cmatrix"
+"sakura"
+"bash-completion"
+"flameshot"
+"alsa-utils"
+"cups"
+"cups-pdf"
+"bluez"
+"bluez-alsa"
+"gvfs"
+"gvfs-mtp"
+"android-tools"
+"noto-fonts-emoji"
+"noto-fonts-cjk"
+"terminus-font"
+"font-inconsolata-otf"
+"gnome-themes-extra"
+"papirus-icon-theme"
+"papirus-folders"
 "neofetch"
 "neovim"
 "tmux"
 "virt-manager"
-"lua5.2"
+"lua"
 "php8.3"
 "php8.3-mysql"
 "sassc"
-)
+"cmake"
+"ninja"
+"meson"
+"ImageMagick"
+"clang"
+"pkgconf-dev"
+"python3-pip"
+"gdb"
+"cross-arm-none-eabi"
+"openjdk"
+) # Lista de pacotes
 
 # Atualiza o sistema
-sudo apt update && sudo apt upgrade -y
-
-if [ "$?" != "0" ]; then
-    echo "Deu ruim... Saindo..."
-    exit 1
-fi
+sudo xbps-install -Syu
 
 # Instala os pacotes necessários
-sudo apt install -y ${PACKAGES[@]}
-
-if [ "$?" != "0" ]; then
-    echo "Deu ruim... Saindo..."
-    exit 1
-fi
+sudo xbps-install -y ${PACKAGES[@]}
 
 # Copia arquivos de configuração para seu locais apropriados
 mkdir -p $HOME/.config
 cp -r config/* $HOME/.config/
 
-if [ "$?" != "0" ]; then
-    echo "Deu ruim... Saindo..."
-    exit 1
-fi
+# Cria diretório do suckless
+mkdir -p $SUCKLESS_DIR
 
+# Builda programas suckless
+for prog in dwm dmenu st dwmblocks slock; do
+    PROG_PATH="$SUCKLESS_DIR/$prog"
+    if [ ! -d "$PROG_PATH"]; then
+        echo "Download $prog..."
+        git clone "https://git.suckless.org/$prog"
+        cd "$PROG_PATH"
+        echo "Build $prog..."
+        make
+        sudo make clean install
+    else
+        echo "$prog is installed... pass.."
+    fi
+done
+
+# Cria diretório de scripts
 mkdir -p $HOME/.local/bin
 cp -r bin/* $HOME/.local/bin/
-
-if [ "$?" != "0" ]; then
-    echo "Deu ruim... Saindo..."
-    exit 1
-fi
 
 for f in bash_aliases gitconfig tmux.conf; do
 	cp $f "$HOME/.${f##/}"
 done
 
-if [ "$?" != "0" ]; then
-    echo "Deu ruim... Saindo..."
-    exit 1
-fi
+# Limpa pacotes residuais
+sudo xbps-remove -oOy
 
-# Baixa wallpapers
-if [ ! -d "$WALLPAPERS_FOLDER" ]; then
-    git clone --depth=1 "$URL_WALLPAPERS" "$WALLPAPERS_FOLDER"
-fi
+# Reconfigura todos os pacotes
+sudo xbps-reconfigure -fa
 
-if [ "$?" != "0" ]; then
-    echo "Deu ruim... Saindo..."
-    exit 1
-fi
-
-# Baixa fontes
-if [ ! -d "$FONTS_FOLDER" ]; then
-    git clone --depth=1 "$URL_FONTS" "$FONTS_FOLDER"
-fi
-
-if [ "$?" != "0" ]; then
-    echo "Deu ruim... Saindo..."
-    exit 1
-fi
-
-# Atualiza cache das fontes
+# Atualiza cache de fontes
 fc-cache -fv
 
-if [ "$?" != "0" ]; then
-    echo "Deu ruim... Saindo..."
-    exit 1
-fi
+# Habilita firewall
+sudo ufw enable
 
-# Limpa pacotes residuais
-sudo apt autoremove -y && sudo apt autoclean && sudo apt clean
+# Habilita serviços essenciais
+sudo ln -s /etc/sv/dbus /var/service/
+sudo ln -s /etc/sv/elogind /var/service/
+sudo ln -s /etc/sv/polkit /var/service/
+sudo ln -s /etc/sv/ufw /var/service/
 
-if [ "$?" != "0" ]; then
-    echo "Deu ruim... Saindo..."
-    exit 1
-fi
+# Remove ttys não usados
+for tty in tty3 tty4 tty5 tty6; do
+    sudo rm -f /var/service/agetty-$tty
+done
+
+# GRUB timeout
+sudo sed -i 's/^GRUB_TIMEOUT=.*/GRUB_TIMEOUT=0/' /etc/default/grub
+sudo grub-mkconfig -o /boot/grub/grub.cfg # Recria arquivo de configuração
 
 # Reinicia o Sistema
 sudo reboot
